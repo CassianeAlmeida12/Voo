af91985d3f3c1a8196f58f17fd679be72c4498b3
Cassiane Almeida <cassiane.almeida@estudante.ifro.edu.br>
2023-06-06 15:27:09 -0400

    Teste voo

---
+++ voo/Program.cs
@@ -0,0 +1,13 @@
+﻿Voo v = new Voo(01, new DateTime(2023, 02, 18));
+
+for(int i = 0; i < v.cadeiras.Length; i++)
+{
+    if (v.cadeiras[i] == true)
+    {
+        Console.WriteLine(v.cadeiras[i] + "esta cadeira está ocupada");
+    }
+    else
+    {
+        Console.WriteLine("Esta cadeira está dispolivel");
+    }
+}
---
+++ voo/Voo.cs
@@ -0,0 +1,28 @@
+﻿public class Voo
+{
+    public int numeroVoo;
+    public DateTime data;
+    public bool[] cadeiras = new bool[100];
+    
+
+    public Voo(int numeroVoo, DateTime data)
+    {
+        this.numeroVoo = numeroVoo;
+        this.data = data;
+    }   
+
+    public DateTime GetData()
+    {
+        return this.data;
+    }
+
+    public int GetNumeroVoo()
+    {
+        return numeroVoo;
+    }
+    public void SetNumeroVoo(int numeroVoo)
+    {
+        this.numeroVoo = numeroVoo;
+    }
+
+}
